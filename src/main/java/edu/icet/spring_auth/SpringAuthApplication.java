b4feb6065cdac5b3d42924a70c08fffad73f8a53
Bhashini123868 <bhashimunasinghe286@gmail.com>
2025-05-20 20:25:40 +0530

    spring security

--- src/main/java/edu/icet/spring_auth/SpringAuthApplication.java
+++ src/main/java/edu/icet/spring_auth/SpringAuthApplication.java
@@ -1,9 +1,11 @@
 package edu.icet.spring_auth;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringAuthApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(SpringAuthApplication.class, args);
     }
 }
--- src/main/java/edu/icet/spring_auth/controller/PublicController.java
+++ src/main/java/edu/icet/spring_auth/controller/PublicController.java
@@ -1,4 +1,14 @@
 package edu.icet.spring_auth.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
 public class PublicController {
+    @GetMapping
+    public String getHello(){
+        return "Hello";
+    }
 }
